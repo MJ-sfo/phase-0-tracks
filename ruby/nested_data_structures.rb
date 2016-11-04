bf4efb80c0f093552eca714fc02b8eab22b376fc
Michael Laird <michaeljlaird@yahoo.com>
2016-11-03 21:30:38 -0700

    started 5.4

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,16 @@
+Church = {
+  worship: {
+    instuments: {
+      guitars: 4,
+      drums: 2,
+      violen: 1,
+      base: 3
+    },
+#  sunday_school: [
+ #   "teachers",
+  #  "paint",
+   # candy
+    #]
+  }
+}
+Church[:worship][:instuments][:guitars]
