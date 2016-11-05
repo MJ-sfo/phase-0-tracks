4aa5db2583b2bb4f902fe7fc98cfca35eff56a60
Michael Laird <michaeljlaird@yahoo.com>
2016-11-04 18:12:10 -0700

    created a nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -6,11 +6,26 @@ Church = {
       violen: 1,
       base: 3
     },
-#  sunday_school: [
- #   "teachers",
-  #  "paint",
-   # candy
-    #]
-  }
-}
-Church[:worship][:instuments][:guitars]
+    sunday_school: [
+      "teachers",
+      "paint",
+      "candy"
+    ]
+  },  
+
+  pastors: [
+      Dave: {
+        kids: 3,
+        worship_ld: TRUE,
+        degree: TRUE
+      },
+      Samantha: {
+        kids: 0,
+        worship_ld: FALSE,
+        degree: TRUE
+      }
+  ]
+}  
+
+p Church[:worship][:instuments][:guitars]
+Church[:worship][:sunday_school].reverse_each {|x| print x, ",  "}
