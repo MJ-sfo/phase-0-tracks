d9f6e8a3e2bec44ae88e326f732dd426b49a6b86
Michael Laird <michaeljlaird@yahoo.com>
2016-11-06 17:32:02 -0800

    replace all letters

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,6 +10,9 @@ real_name = gets.chomp
 
 stringarray = real_name.split("")
 vowels_notcaps = ['a', 'e', 'i', 'o', 'u']
+vowels_caps = ['A', 'E', 'I', 'O', 'U']
+consonants_caps = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+consonants_notcaps = ['B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
 
 new_name = stringarray.each_with_index.map{|letter, index|
     if vowels_notcaps.include?(letter)
@@ -22,8 +25,39 @@ new_name = stringarray.each_with_index.map{|letter, index|
     else
       letter
     end
-  }
 
+    if vowels_caps.include?(letter)
+      new_index = vowels_caps.index(letter)
+      if new_index == 4
+        letter = vowels_caps[0]
+      else
+        letter = vowels_caps[new_index+1]
+      end
+    else
+      letter
+    end
 
+    if consonants_notcaps.include?(letter)
+      new_index = consonants_notcaps.index(letter)
+      if new_index == 20
+        letter = consonants_notcaps[0]
+      else
+        letter = consonants_notcaps[new_index+1]
+      end
+    else
+      letter
+    end
+    
+    if consonants_caps.include?(letter)
+      new_index = consonants_caps.index(letter)
+      if new_index == 20
+        letter = consonants_caps[0]
+      else
+        letter = consonants_caps[new_index+1]
+      end
+    else
+      letter
+    end
+  }    
 real_name = new_name.join("")
 p real_name
