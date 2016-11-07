bed07bd30953a52d441fc60cbe83c7e173f8d1cb
Michael Laird <michaeljlaird@yahoo.com>
2016-11-06 21:47:11 -0800

    finished second part - Fibonacci

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -12,4 +12,30 @@ end
 arr_test = [42, 89, 23, 5]
 search_array(arr_test, 23)
 
+# Release 1: Calculate Fibonacci Numbers
+def fib(input)
+  output = []
+  if input == 1 
+    then output = [0]
+  elsif input == 2 
+    then output = [0, 1]
+  else
+    output = [0, 1]
+    loop = 2
+    while loop < input
+      output = output.push(output[loop-2] + output[loop - 1])
+      loop += 1
+    end
+  end
+  p output
+end
+
+p "What Fibonacci number do you want?"
+fib_num = gets.chomp.to_i 
+if fib_num < 1 then
+  p "must be a positive Integer."
+else
+  fib_num = (fib_num / 1) 
+  fib(fib_num)
+end
 
