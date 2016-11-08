1a01cdf4edfc6628d09f6da3f26c51052bee8fd0
Michael Laird <michaeljlaird@yahoo.com>
2016-11-08 15:59:56 -0800

    characteristics and behavior of a puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+SPECIES ------------------------
+Cutius Temporarious
+
+CHARACTERISTICS ----------------
+Friendly: yes
+Fur_length: short
+bite: yes
+Name: Rover
+Tail: wagging
+
+
+
+BEHAVIOR -----------------------
+Cries
+Eats
+Runs
