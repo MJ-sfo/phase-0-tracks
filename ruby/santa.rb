eb295376b1301941774a8087f660312b3dd89cb7
Michael Laird <michaeljlaird@yahoo.com>
2016-11-08 20:39:11 -0800

    started santa class

---
+++ ruby/santa.rb
@@ -0,0 +1,25 @@
+# print "Ho, ho, ho! Haaaappy holidays!" 
+def speak
+  p "Ho, ho, ho! Haaaappy holidays!" 
+end
+
+# prints "That was a good <type of cookie 
+def eat_milk_and_cookies(cookie)
+  puts "That was a good #{cookie} cookie!" 
+end
+
+# prints "Initializing Santa  
+  puts "Initializing Santa instance ..."
+end
+
+# Release 0: Bring Santa to Life
+class Santa
+  def initialization(gender)
+    @gender = gender
+  end
+  def ethnicity(ethnct)
+    @ethnct = ethnct
+  end
+  reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  age = 0 
+end
