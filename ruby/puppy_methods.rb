492129d53e71d757cbf98c76c86aa8629d485aff
Michael Laird <michaeljlaird@yahoo.com>
2016-11-10 10:10:17 -0800

    finished all three parts of 6.2 with paired partner Yusuke

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,6 +1,9 @@
 #  1 - Add driver -instance of Puppy obeying commands
 class Puppy
    attr_accessor :fetch
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
   def ball
       puts "Puppy fetch ball !"
   end
@@ -27,3 +30,35 @@ try_it.ball
 try_it.roll_over
 puts "our puppy is really #{try_it.dog_years(2)} years old"
 try_it.sit
+
+
+#initialize is called when the instance was generated.
+#If the name of the initialize method is misspelled, the method doesn't run when the instance was generated.
+
+class Turtle
+  attr_accessor :size
+  def initialize
+    # puts "generated instance!"
+  end
+
+  def mysize
+    @size = "large"
+    puts "size is called"
+  end
+
+  def scare
+    puts "afraid of height!!"
+  end
+
+end
+
+instance_array = []
+
+50.times do |number|
+  instance_array << Turtle.new
+end
+
+instance_array.each do |instance|
+  instance.mysize
+  instance.scare
+end
