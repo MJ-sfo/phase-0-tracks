a44f9c12ddb01df7349cdeb931e9eef41e5755e4
Michael Laird <michaeljlaird@yahoo.com>
2016-11-09 19:10:47 -0800

    Initialized Santas method, but needs more work

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,4 @@
+# Release 0: Bring Santa to Life
 # print "Ho, ho, ho! Haaaappy holidays!" 
 def speak
   p "Ho, ho, ho! Haaaappy holidays!" 
@@ -9,17 +10,32 @@ def eat_milk_and_cookies(cookie)
 end
 
 # prints "Initializing Santa  
+def print_santa
   puts "Initializing Santa instance ..."
 end
 
-# Release 0: Bring Santa to Life
+# Release 1: Give Santa Attributes for Christmas
 class Santa
-  def initialization(gender)
+  def example_genders(gender)
     @gender = gender
   end
-  def ethnicity(ethnct)
+  def example_ethnicities(ethnct)
     @ethnct = ethnct
   end
   reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   age = 0 
 end
+
+#  initialization code 
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+example_genders.length.times do |i|
+#  santas << Santa.new(example_genders[i], example_ethnicities[i])
+  temp_santa = Santa.new
+  temp_santa.example_genders(example_genders[i])
+  temp_santa.example_ethnicities(example_ethnicities[i])
+  santas << temp_santa
+end
+p santas
