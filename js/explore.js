bd7c045ef3a7a837f2e8395af3a2f665783abfd6
Michael Laird <michaeljlaird@yahoo.com>
2016-11-18 10:55:35 -0800

    finished pseudocode

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// Release 4: Write a JavaScript Program
+// pseudocode a function .reverse
+
+// create method that accepts a text
+// set counting variable equal to half length of text -minus one if length is odd number
+// create a loop - half length of text 
+// take first character, put into holding-variable, put last character in first place
+// put first character in last place
