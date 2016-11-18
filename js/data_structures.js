5c5799bd77d6797354658da2731e3b83b5df98aa
Michael Laird <michaeljlaird@yahoo.com>
2016-11-18 11:50:25 -0800

    finished first two parts of 7.2 with Noelle

---
+++ js/data_structures.js
@@ -0,0 +1,19 @@
+var blue_array = ["sky blue", "teal", "navy blue" , "barbie pink"];
+
+blue_array.push("red");
+
+console.log(blue_array);
+
+var name_array = [ "lucky", "stumpy" , "wizard of oz" , "aladdin"];
+
+name_array.push("queenie");
+
+console.log(name_array);
+
+// var horse = {color: blue_array, name: name_array};
+
+for (var i = 0; i < name_array.length; i++){
+    console.log("name and color: " + name_array[i], blue_array[i]);
+};
+// horse.push(blue_array[1],name_array[1]);
+// console.log(horse);
