7d84b49ec0fff28231eb2858adfe737ce08c7ee8
Michael Laird <michaeljlaird@yahoo.com>
2016-11-18 18:43:14 -0800

    finished cars, and 7.2

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,38 +6,21 @@ var name_array = [ "lucky", "stumpy" , "wizard of oz" , "aladdin"];
 name_array.push("queenie");
 console.log(name_array);
 
-// var horse = {color: blue_array, name: name_array};
-
 for (var i = 0; i < name_array.length; i++){
     console.log("name and color: " + name_array[i], blue_array[i]);
 };
-// horse.push(blue_array[1],name_array[1]);
-// console.log(horse);
 
-// Tried with Noelle to create a hash-like object and push info into
-// var inventory = [];
+
+// Tried with Noelle to create a hash-like object and push new info into the object.  could not push except into arrays.
 // // created new 'car' object
 // var car = {
 //   car_class: 'SUV',
 //   color: 'black',
 //   automatic: true
 // };
+console.log("---------------------");
 
-// //  constructor function for a car.
-// function carAdd(classCar, clr, transmission) {
-//  this.classCar = classCar;
-//  this.clr = clr;
-//  this.transmission = transmission;
-//  // car_add.push 
-//  inventory.push({car_class: classCar, color: clr, automatic: transmission });
-//  car.push({car_class: classCar, color: clr, automatic: transmission });
-// }; // function does NOT end with semicolon
-// // person.push({age: 35, name: "John Doe"}); // Array now contains 1 item
-// carAdd('convertable', 'green', false);
-// console.log(car);
-// console.log(inventory);
-
-// now will try by making car in two arrays, and pushing filled info into object
+// now will try by making car in two arrays
 var  car_class = ['SUV'];
 var  color = ['black'];
 var  automatic = [true];
@@ -53,13 +36,9 @@ carAdd('mini', 'red', true);
 carAdd('limo', 'white', true);
 
 for (var i = 0; i < car_class.length; i++){
-    console.log("This car is a " + color[i] + " " + car_class[i]);
+    if (automatic[i]) {
+      console.log("This car is a " + color[i] + " automatic " + car_class[i]);
+    } else {
+      console.log("This car is a " + color[i] + " standard " + car_class[i]);
+    }    
 };
-
-// speak: function() { 
-//       if (this.wantsCracker) {
-//         console.log("Polly wants a cracker!");
-//       } else {
-//         console.log("Hello! Hello!");
-//       }
-//     }
