5b9effc1e60442eb85d0b9cc722cc9c61b3b6258
Michael Laird <michaeljlaird@yahoo.com>
2016-11-20 11:10:32 -0800

    counts lenght of characters of string, makes loop for first half of characters

--- js/explore.js
+++ js/explore.js
@@ -6,10 +6,20 @@
 // create a loop - half length of text 
 // take first character, put into holding-variable, put last character in first place
 // put first character in last place
-var nonReverse = "";
+
+// var nonReverse = "";
 function reverse(nonReverse) {
-  / math.floor rounds DOWN to closest integer
-  var characters = Math.floor(nonReverse.length / 2);
-  console.log("length of word is " + nonReverse.length + " and half is: " + characters); 
+  // math.floor rounds DOWN to closest integer
+  var num_characters = Math.floor(nonReverse.length / 2);
+  var hold_letter = ""
+  console.log("length of word is " + nonReverse.length + " and half is: " + num_characters); 
+  for (i = 0; i < num_characters; i++) {
+    hold_letter = nonReverse[i];
+    console.log("the letter " + i + " is: " + hold_letter);
+    console.log("the letter to be moved forard is: " + nonReverse[nonReverse.length-i-1])
+    // nonReverse[i] = nonReverse[nonReverse.length-i-1];
+    // nonReverse[nonReverse.length-i] = hold_letter;
+  };  // for loop - only runs iftext has more than one letter
+  console.log("the reorginized word is: " + nonReverse);
 }
-reverse("y");
+reverse("Mike");  // any loop must make sure that length is greater than one
