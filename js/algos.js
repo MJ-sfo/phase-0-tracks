649d2f4c7c1ed0dd679d7d8214086e5c9e5947cb
Michael Laird <michaeljlaird@yahoo.com>
2016-11-20 14:15:01 -0800

    psudocode for first part of 7.3

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+// Release 0: Find the Longest Phrase
+// build function that takes array as paramater
+// find length of array
+// build vars for length of each item and the index in array
+// check the arra isn't empty
+// if only one element, work is done
+// build loop - cycles number of items minus one
+// each loop - compare next item for lenght.  if longer than record, record index
+// after loop, return result of longest item
+// driver code, that tests several arrays
+
