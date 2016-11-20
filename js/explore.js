561d5ae418f48f36ccb4ccb6d22467919b66322b
Michael Laird <michaeljlaird@yahoo.com>
2016-11-20 13:06:00 -0800

    finished loop that replaces every character in string

--- js/explore.js
+++ js/explore.js
@@ -7,18 +7,24 @@
 // take first character, put into holding-variable, put last character in first place
 // put first character in last place
 
-// var nonReverse = "";
 function reverse(nonReverse) {
   // math.floor rounds DOWN to closest integer
   var num_characters = Math.floor(nonReverse.length / 2);
+  var length_word = nonReverse.length
   var hold_letter = ""
-  console.log("length of word is " + nonReverse.length + " and half is: " + num_characters); 
+  var newWord = nonReverse
+  console.log("length of word is " + length_word + " and half is: " + num_characters); 
   for (i = 0; i < num_characters; i++) {
-    hold_letter = nonReverse[i];
-    console.log("the letter " + i + " is: " + hold_letter);
-    console.log("the letter to be moved forard is: " + nonReverse[nonReverse.length-i-1])
-    // nonReverse[i] = nonReverse[nonReverse.length-i-1];
-    // nonReverse[nonReverse.length-i] = hold_letter;
+    var first_letter = nonReverse[i];
+    var last_letter = nonReverse[length_word-i-1];
+    console.log("the letter " + i + " to be moved forward is: " + first_letter);
+    console.log("the letter to be moved back is: " + last_letter);
+    // i have to create a function, that changes a string by index
+    String.prototype.replaceAt=function(index, character) {
+      return this.substr(0, index) + character + this.substr(index+character.length);
+      } ;   
+    nonReverse = nonReverse.replaceAt(i, last_letter);
+    nonReverse = nonReverse.replaceAt((length_word-i-1), first_letter);
   };  // for loop - only runs iftext has more than one letter
   console.log("the reorginized word is: " + nonReverse);
 }
