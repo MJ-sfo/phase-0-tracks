abd3136377f731b5f6c65889db1bcc6a1ad2b838
Michael Laird <michaeljlaird@yahoo.com>
2016-11-20 18:34:06 -0800

    found length of array

--- js/algos.js
+++ js/algos.js
@@ -1,11 +1,17 @@
 // Release 0: Find the Longest Phrase
-// build function that takes array as paramater
+// build function that takes array as parameter
 // find length of array
 // build vars for length of each item and the index in array
-// check the arra isn't empty
+// check the array isn't empty
 // if only one element, work is done
 // build loop - cycles number of items minus one
-// each loop - compare next item for lenght.  if longer than record, record index
+// each loop - compare next item for length.  if longer than record, record index
 // after loop, return result of longest item
 // driver code, that tests several arrays
 
+function longest_word(array_list) {
+  console.log(array_list.length)
+}
+
+test_array = ["help", "life", "David's home"];
+longest_word(test_array);
