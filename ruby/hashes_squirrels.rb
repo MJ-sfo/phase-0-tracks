98c2b5946e620a5c4ea628a0308b9924cbbf8ac8
Michael Laird <michaeljlaird@yahoo.com>
2016-11-25 13:22:13 -0800

    created pseudocode for 5.2

---
+++ ruby/hashes_squirrels.rb
@@ -0,0 +1,10 @@
+# pseudocode
+# loop through array of keys
+# ask user for name of key,  input
+# test type of input, create data type
+# push key pair into hash
+# when user marks done, stop loop, print hash
+# ask user for any key use wants changed
+# change key to new input
+# when user marks "none" stop, print updated hash
+
