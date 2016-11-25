172396d388d52f6dc2bd9441d3f7fdba0c895d86
Michael Laird <michaeljlaird@yahoo.com>
2016-11-25 15:23:37 -0800

    improved pseudocode for 5.2

--- ruby/hashes_squirrels.rb
+++ ruby/hashes_squirrels.rb
@@ -1,10 +1,11 @@
 # pseudocode
-# loop through array of keys
-# ask user for name of key,  input
-# test type of input, create data type
-# push key pair into hash
-# when user marks done, stop loop, print hash
-# ask user for any key use wants changed
-# change key to new input
+# create a hash
+# loop through the hash
+# ask user for name for input
+# convert, if necessary, input into correct data type, push into hash
+# stop loop, print hash
+# ask user for any key the paired value the user wants the value changed
+# convert input string for key into symbol for key.
+# change new input for that key
 # when user marks "none" stop, print updated hash
 
