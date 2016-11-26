72689bfb3baa38c7c57b3f628d0d0b4ef5849c7d
Michael Laird <michaeljlaird@yahoo.com>
2016-11-26 14:38:00 -0800

    finished Release 1

--- ruby/iteration_squirrles.rb
+++ ruby/iteration_squirrles.rb
@@ -2,14 +2,38 @@
 # Release 1: Use each, map, and map!Release 1: Use each, map, and 
 # map!Release 1: Use each, map, and map!
 
-# { |thing_to_print| puts thing_to_print }
-# do |thing_to_print|
-#   puts thing_to_print
-# end
 sublings = ["Anne", "Jen", "Tim"]
 family = {
   aunt: ['Nancy','Eileen', 'Katie'],
   uncle: ['Jim', 'Bob','Dick'],
-  dick: {Polla: "Twenty dogs", karen: ["Three dogs", 'two kids']},
+  dick: {polla: "Twenty dogs", karen: ["Three dogs", 'two kids']},
   art: "two kids"
 }
+sublings.each do |item|
+  # puts "Family member #{sublings[item]}." #- doesn't work
+  puts "Array Family member #{item}."
+end
+
+family.each do |item|
+  # puts "Family member #{sublings[item]}." #- doesn't work
+  puts "hash Family member #{item}."
+end
+puts "----------------------"
+sublings.map do |item|
+    puts "Array Family member #{item}."
+  item.next     #  same as sublings = sublings.next
+end
+puts "After .map call: #{sublings}"
+
+sublings.map! do |item|
+    puts "Array Family member #{item}."
+  item.next    
+end
+puts "After .map! call: #{sublings}"
+puts sublings
+puts "----------------------"
+family.map do |item|
+  puts "Array Family member #{item}."
+  # item.next    - this works on array but not on hash
+end
+puts family
