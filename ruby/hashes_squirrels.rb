78a92dd83f8d8befc122c8194f4f475e2345d8be
Michael Laird <michaeljlaird@yahoo.com>
2016-11-25 22:19:35 -0800

    the .to_sym function turns text into symbol for key - final part i was looking for

--- ruby/hashes_squirrels.rb
+++ ruby/hashes_squirrels.rb
@@ -12,6 +12,16 @@
 # client.each do |key, data|    # this prints VALUES
 #   puts "What do you wish to enter for #{client[key]}?"
 # end
+client = {
+  name: "",
+  age:  0,
+  number_kids: 0,
+  decor: "",
+  paid_advanced: false
+}
+# client.each do |key, data|    # this prints VALUES
+#   puts "What do you wish to enter for #{client[key]}?"
+# end
 input = ""
 for key in client.keys.sort
   if key.to_s == "paid_advanced"
@@ -24,7 +34,36 @@ for key in client.keys.sort
     puts "What do you wish to enter for #{(key.inspect).reverse.chop.reverse }?"  # .reverse.chop.reverse is to remove colon
     input = gets.chomp
     p input
+    if key.to_s == "number_kids" or key.to_s == "age"
+      client[key] = input.to_i
+    else
+      client[key] = input
+    end
   end
 end
 puts "#{client}"
 
+finish = false
+input_update = ""
+while finish == false
+  puts "Which data would you like to update? (type 'none' if satisfied)"
+  update = gets.chomp.strip.downcase
+  if update == "none" or update == ""
+    finish = true
+  else
+    puts "What is the new value for #{update}?"
+    input_update = gets.chomp.strip
+    if update == "paid_advanced"
+      if input_update[0] == "y" or input_update[0] == "t"
+        client[update.to_sym] = true
+      else
+        client[update.to_sym] = false
+      end
+    elsif update  == "number_kids" or update  ==  "age"
+      client[update.to_sym] = input_update.to_i
+    else
+      client[update.to_sym] = input_update
+    end
+  end
+end
+puts "#{client}"
