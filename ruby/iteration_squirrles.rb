9f6a0b587a6d725f902c41c902ec10f567a4ee28
Michael Laird <michaeljlaird@yahoo.com>
2016-11-26 10:50:37 -0800

    created iteration file for 5.3

---
+++ ruby/iteration_squirrles.rb
@@ -0,0 +1,15 @@
+
+# Release 1: Use each, map, and map!Release 1: Use each, map, and 
+# map!Release 1: Use each, map, and map!
+
+# { |thing_to_print| puts thing_to_print }
+# do |thing_to_print|
+#   puts thing_to_print
+# end
+sublings = ["Anne", "Jen", "Tim"]
+family = {
+  aunt: ['Nancy','Eileen', 'Katie'],
+  uncle: ['Jim', 'Bob','Dick'],
+  dick: {Polla: "Twenty dogs", karen: ["Three dogs", 'two kids']},
+  art: "two kids"
+}
