d67574d61df4042677d8658e47c211cf937ad853
Michael Laird <michaeljlaird@yahoo.com>
2016-11-27 10:17:01 -0800

    pseudocode for Release 1

--- ruby/problem_solving_methods_squirrels.rb
+++ ruby/problem_solving_methods_squirrels.rb
@@ -4,7 +4,7 @@
 # run loop on array - compare each item with integer
 # if match, make integer = var
 #   return val of var
-  
+
 def find_integer(array_in, int_to_find)
   var = nil
   array_in.each do |index|
@@ -24,3 +24,11 @@ end
 
 use_this_array = [1,5,17,25]
 puts "we found the integer at #{find_integer(use_this_array, 5 )} spot in array"
+
+# Release 1: Calculate Fibonacci Numbers
+# define method - take one Number
+# reduce number to floor integer
+# if less than 0, send error msg
+#   integer 0,1,2 return prest array
+#   loop - three through length array - push new item, sum of last two items of array
+#   return final array 
