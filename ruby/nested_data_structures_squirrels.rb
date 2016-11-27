88fb4dff924ed58371630bcb02fa44711cbbd2f6
Michael Laird <michaeljlaird@yahoo.com>
2016-11-26 19:11:26 -0800

    printed from various layers of nested data

--- ruby/nested_data_structures_squirrels.rb
+++ ruby/nested_data_structures_squirrels.rb
@@ -1,5 +1,16 @@
 family = {
   aunt: ['Nancy','Eileen', 'Katie'],
-  uncle: ['Jim', 'Bob',dick: {polla: "Twenty dogs", karen: ["Three dogs", 'two kids']}],  
+  uncle: ['Jim', 'Bob', dick: {
+                                polla: "Twenty dogs", 
+                                karen: ["Three dogs", 'two kids']
+                                }], 
   art: "two kids"
 }
+puts family
+puts "I have three aunts: #{family[:aunt]}"
+puts "My uncle #{family.key("two kids")} has #{family[:art]}."
+p "--------------------"
+puts family[:uncle]
+p "--------------------"
+puts "My Uncle Dick has the largest family: #{family[:uncle][2]}"
+family[:aunt]
