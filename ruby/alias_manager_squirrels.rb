fcdc79ac748ea30686afabbaf410f86349044425
Michael Laird <michaeljlaird@yahoo.com>
2016-11-26 19:32:33 -0800

    created pseudocode - Release 0

---
+++ ruby/alias_manager_squirrels.rb
@@ -0,0 +1,10 @@
+# Release 0: Attempt a Tricky Algorithm
+# pseudocode
+# create variable with spy name
+# split name into array of words, reverse array, join back into one name
+# split new name into array of letters.
+# lowercase letters
+# do each loop
+# if statement - letter match a vowel, move up one letter in vowel
+# else - move up one letter in consonant
+# finally - join array into one words
