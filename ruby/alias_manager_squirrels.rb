7e96be456b9e4d7a6a063d00dd1a685b39589e05
Michael Laird <michaeljlaird@yahoo.com>
2016-11-26 21:06:33 -0800

    reversed spy name and broke into array, to prepare for loop

--- ruby/alias_manager_squirrels.rb
+++ ruby/alias_manager_squirrels.rb
@@ -2,9 +2,18 @@
 # pseudocode
 # create variable with spy name
 # split name into array of words, reverse array, join back into one name
-# split new name into array of letters.
+# reverse name, break into array
 # lowercase letters
 # do each loop
-# if statement - letter match a vowel, move up one letter in vowel
-# else - move up one letter in consonant
+# if statement - letter match a vowel, if edge letter change to a, else move up one letter in vowel
+# else - if edge letter change to b, else move up one letter in consonant
 # finally - join array into one words
+
+name = "Felicia Torres" 
+name_array = name.split('')
+name_array = name_array.reverse
+name_backwords = name_array.join('')
+p name_backwords
+
+name_arra = name_backwords.downcase.split('')
+p name_arra
