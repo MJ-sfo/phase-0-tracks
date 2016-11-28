7c075d37c53b2fbbb968b34d6d8a122feedc52e5
Michael Laird <michaeljlaird@yahoo.com>
2016-11-28 15:41:15 -0800

    created class  of Puppy

--- ruby/puppy_squirrels.rb
+++ ruby/puppy_squirrels.rb
@@ -3,3 +3,17 @@
 
 # Release 1: Declare Your Class and Create an Instance in IRB
 # Release 2: Practice Using Class Methods and Instance Methods
+class Puppy
+  def fly
+    puts "I keep throwing up Puppy, but he doesn't fly !"
+  end
+  def size
+    puppy_size = "lap-sized"
+  end
+  def fun3
+    p "Woooza!"
+  end
+  def dance
+    dance = "on hind legs"
+  end
+end
