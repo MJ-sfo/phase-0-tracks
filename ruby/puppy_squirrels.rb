07532615404e2f6530d2feded798c6cde6799f53
Michael Laird <michaeljlaird@yahoo.com>
2016-11-28 14:57:58 -0800

    created puppy_squirrels.rb

---
+++ ruby/puppy_squirrels.rb
@@ -0,0 +1,5 @@
+# 6.1 Introduction to Classes
+#  a Puppy class - fly, size, fun, dangerous, dances
+
+# Release 1: Declare Your Class and Create an Instance in IRB
+# Release 2: Practice Using Class Methods and Instance Methods
