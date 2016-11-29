136f7970e19855e1c1b83729d70def4cf6572388
Michael Laird <michaeljlaird@yahoo.com>
2016-11-29 12:35:30 -0800

    created tricks for puppy

--- ruby/puppy_methods_squirrels.rb
+++ ruby/puppy_methods_squirrels.rb
@@ -5,8 +5,22 @@ class Puppy
     @dogs_name = name
   end
   
-  def give_name(name)
+  def give_name(name)  # we created this method
     @dogs_name = name
+  end
+  
+  def speak(barking)
+    barking.times do  
+      puts "Woof!"
+    end
+  end
+  
+  def dog_years(human_years)
+    dog_years = human_years * 7
+  end
+
+  def roll_over 
+    puts "*rolls over*"
   end
 
   def fetch(toy)
@@ -18,3 +32,6 @@ end
 turn_on_class = Puppy.new
 turn_on_class.fetch("ball")
 puts "our dog's name is #{turn_on_class.give_name("Spot")}."
+turn_on_class.speak(5)
+turn_on_class.roll_over
+puts "our dog is really #{turn_on_class.dog_years(2)}."
