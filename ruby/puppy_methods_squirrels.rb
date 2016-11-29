f090e64d299f42408ecd1f36c3f6ca427442bc6f
Michael Laird <michaeljlaird@yahoo.com>
2016-11-29 12:23:04 -0800

    Add driver code at the bottom that initializes an instance of Puppy

--- ruby/puppy_methods_squirrels.rb
+++ ruby/puppy_methods_squirrels.rb
@@ -1,10 +1,20 @@
 # Release 0: Add Behavior to a Class
 
 class Puppy
+  def instance
+    @dogs_name = name
+  end
+  
+  def give_name(name)
+    @dogs_name = name
+  end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
-    toy
+    # toy
   end
 
 end
+turn_on_class = Puppy.new
+turn_on_class.fetch("ball")
+puts "our dog's name is #{turn_on_class.give_name("Spot")}."
