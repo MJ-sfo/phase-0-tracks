7b55fa2a227561dba1829b20ae1a9934b0b9c746
Michael Laird <michaeljlaird@yahoo.com>
2016-11-28 19:16:07 -0800

    created class for Santa, need to fix

--- ruby/santa_squirrels.rb
+++ ruby/santa_squirrels.rb
@@ -2,3 +2,17 @@
 # defining a Santa class 
 #   three instance methods: speak , eat_milk_and_cookies , initialize 
 
+class Santa 
+  def initialize
+    puts  "Initializing Santa instance ...".
+  end
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!" 
+    puts 'instance method'
+  end
+
+  def eat_milk_and_cookies(eat)
+    puts  "That was a good #{eat}!" 
+  end
+end
