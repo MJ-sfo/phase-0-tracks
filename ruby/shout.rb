11039f3b21beca40717ea781f46ec522d78cec36
Michael Laird <michaeljlaird@yahoo.com>
2016-11-30 09:43:35 -0800

    added 'yelling_happily' to module Shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,40 +1,11 @@
 # Release 1: Write a Simple Module
-# module Shout
-#   def initialize
-#     @self = ""
-#   end
-#   def self.yell_angrily(words)
-#     words + "!!!" + " :("
-#   end
-#   def self.yelling_happily(words)
-#     words + " amen!!!" + " :)" 
-#   end 
-# end
 
-# p Shout.yell_angrily "I like cheese"
-# p Shout.yelling_happily "We won the House"
-
-# Release 3: Convert a Standalone Module to a Mixin
-# module - to be included in class(es)
 module Shout
-  def yell_angrily(words)
+  def self.yell_angrily(words)
     words + "!!!" + " :("
   end
-  def yelling_happily(words)
-    words + " amen!!!" + " :)" 
-  end 
-end
-# class - includes module
-class Angry
-  include Shout
-end
 
-class Gambler
-  include Shout
+  def self.yelling_happily
+    puts "Thank you Lord!"
+  end
 end
-# instance on class, input for module(s)
-angry = Angry.new
-puts angry.yell_angrily("Learn to drive")
-
-house = Gambler.new
-puts house.yelling_happily("Martha, we won the house")
