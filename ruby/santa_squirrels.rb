2a1b4a4ad3de988884f1d54bcbfa768dc6df58f3
Michael Laird <michaeljlaird@yahoo.com>
2016-11-29 21:54:41 -0800

    finished Release 3: Refactor with attr_reader and attr_accessor

--- ruby/santa_squirrels.rb
+++ ruby/santa_squirrels.rb
@@ -10,7 +10,78 @@
 # Add three attribute-changing methods 
 # Add two "getter" methods
 
+# class Santa 
+#   def initialize(gend, ethn)
+#     puts "Initializing Santa instance ..."
+#     @gender, @ethnicity = gend, ethn
+#     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+#     @age = 0 
+#   end
+
+#   def speak
+#     puts "Ho, ho, ho! Haaaappy holidays!"
+#   end
+
+#   def eat_milk_and_cookies(eat)
+#     # @eat = eat
+#     puts  "That was a good #{eat} cookie !" 
+#   end
+  
+#   def count_reindeer 
+#     count = @reindeer_ranking.length
+#   end
+#   #  Release 2: Change an Attribute with a Method
+#   def celebrate_birthday
+#     @age = @age + 1        # +=1 
+#   end
+  
+#   def get_mad_at(bad_reindeer)
+#     @reindeer_ranking.delete(bad_reindeer) # not: @reindeer_ranking = @reindeer_ranking.delete(bad_reindeer)
+#     @reindeer_ranking << bad_reindeer
+#     # puts "new array is: #{@reindeer_ranking}"
+#   end
+  # a setter method --- but also need a getter method, to use outside of class Santa
+#   def gender_makenew=(new_gender) 
+#     @gender = new_gender
+#   end
+#   def gender
+#     @gender
+#   end
+#   #  two "getter" methods
+#   def age
+#     @age
+#   end
+#   def ethnicity 
+#     @ethnicity 
+#   end
+# end
+# talking = Santa.new("male", "green")
+# talking.speak
+# talking.eat_milk_and_cookies("oats")
+# puts talking.get_mad_at("Dancer")
+# puts "new age of Santa is #{talking.celebrate_birthday}."
+# talking.gender_makenew = "unknown gender"
+# puts "Because of expensive operation, Santa is now #{talking.gender}."
+# puts "Our Santa is #{talking.ethnicity} and #{talking.age} years old."
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+# p santas
+# talking = Santa.new("non-gender","non-ethnic")
+# puts "Santa has #{talking.count_reindeer} reindeer."
+
+# Release 3: Refactor with attr_reader and attr_accessor
+# add attr_reader and attr_accessor
+# remove getter and setter methods 
+
 class Santa 
+  attr_reader :age, :ethnicity
+  attr_accessor :gender
+  
   def initialize(gend, ethn)
     puts "Initializing Santa instance ..."
     @gender, @ethnicity = gend, ethn
@@ -40,27 +111,14 @@ class Santa
     @reindeer_ranking << bad_reindeer
     # puts "new array is: #{@reindeer_ranking}"
   end
-  # a setter method --- but also need a getter method, to use outside of class Santa
-  def gender_makenew=(new_gender) 
-    @gender = new_gender
-  end
-  def gender
-    @gender
-  end
-  #  two "getter" methods
-  def age
-    @age
-  end
-  def ethnicity 
-    @ethnicity 
-  end
 end
+
 talking = Santa.new("male", "green")
 talking.speak
 talking.eat_milk_and_cookies("oats")
 puts talking.get_mad_at("Dancer")
 puts "new age of Santa is #{talking.celebrate_birthday}."
-talking.gender_makenew = "unknown gender"
+talking.gender = "unknown gender"
 puts "Because of expensive operation, Santa is now #{talking.gender}."
 puts "Our Santa is #{talking.ethnicity} and #{talking.age} years old."
 
@@ -73,4 +131,3 @@ end
 p santas
 talking = Santa.new("non-gender","non-ethnic")
 puts "Santa has #{talking.count_reindeer} reindeer."
-
