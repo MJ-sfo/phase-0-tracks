8a0085f304e7ec143670a3a31a46ad796ee3b0f3
Michael Laird <michaeljlaird@yahoo.com>
2016-11-30 14:02:36 -0800

    added method to exit

---
+++ ruby/shout_squirrels.rb
@@ -0,0 +1,17 @@
+# Release 1: Write a Simple Module
+
+# testing in command mode:
+# irb return - get into irb
+# require_relative 'shout_squirrels' - tell it what file to look at (dont need '.rb')
+# Shout.yelling_happily       - Class dot method (cap class)
+# exit  - to get out of irb
+
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily
+    puts "Thank you Lord!"
+  end
+end
