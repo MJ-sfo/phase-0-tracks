692336c142d855043bfa461394f75c0dbb30def2
Michael Laird <michaeljlaird@yahoo.com>
2016-11-29 20:43:48 -0800

    finished Release 1

--- ruby/santa_squirrels.rb
+++ ruby/santa_squirrels.rb
@@ -22,8 +22,22 @@ class Santa
     # @eat = eat
     puts  "That was a good #{eat} cookie !" 
   end
+  
+  def count_reindeer 
+    count = @reindeer_ranking.length
+  end
 end
 talking = Santa.new("male", "green")
 talking.speak
 talking.eat_milk_and_cookies("oats")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+p santas
+talking = Santa.new("non-gender","non-ethnic")
+puts "Santa has #{talking.count_reindeer} reindeer."
 
