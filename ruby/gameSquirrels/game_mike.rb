09c5ca67eb4ad41dddf80106f64b8e0eabf8c1bc
Michael Laird <michaeljlaird@yahoo.com>
2016-12-01 12:54:50 -0800

    turned procedures into class 'Guess_word'

--- ruby/gameSquirrels/game_mike.rb
+++ ruby/gameSquirrels/game_mike.rb
@@ -1,5 +1,5 @@
 # 6-6 Solo Challenge- A Game Class - Release 1-Design a Game
-# psudocode
+# pseudopod
 # enter word
 # strip, lowercase, remove any non-letters
 # "    hello    ".strip   #=> "hello"
@@ -8,12 +8,12 @@
 # str.squeeze(" ")   - removes multi-space
 # .gsub(/[^a-zA-Z]/, '')  - removes non-letters
 
-# create text of dashes, equal to lenght of word
+# create text of dashes, equal to length of word
 # set finish to false
 # loop for guesses - # twice the length of word
 # quit when want
 # guess - strip, take first letter
-# if new guess match previous, give message, restrat loop  
+# if new guess match previous, give message, restart loop  
 # 'hello'.match('xx')         #=> nil  --so if nil, not in 
 # "hello".include? "lo"   #=> true
 # "hello".index('e')             #=> 1 - finds index of FIRST letter,
@@ -28,4 +28,48 @@
 # if no more dashes, congrats, set finish to true
 # add to counter
 # end
-# print end if did not finish guesssing word.
+# print end if did not finish guessing word.
+
+testword = "  i lov(e   a 45 CAM. "
+testword =testword.gsub(/[^a-zA-Z]/, '').downcase
+p testword
+finish = false
+if testword.empty? == true
+  puts"Sorry, must include letters."
+  finish = true
+end
+hidden_word  = "-"*testword.length
+puts hidden_word
+
+class Guess-word
+  attr_accessor :testword, :guess
+  attr_reader :hidden_word
+  
+  def initialize
+    @testword =""
+    @guess = ""
+    @list_guesses = ""
+    @hidden_word = ""
+    @finish = false
+  end
+  
+  def clean_testword
+    @testword =@testword.gsub(/[^a-zA-Z]/, '').downcase
+    if @testword.empty? == true
+      puts "Sorry, must include letters."
+      @finish = true
+    end
+    set_hidden_word
+    puts "Guess the letters to '#{@hidden_word}'"
+  end
+
+  def set_hidden_word
+    @hidden_word  = "-"*@testword.length
+  end
+  
+  def receive_guess
+    @guess = @guess.gsub(/[^a-zA-Z]/, '').downcase[0]
+    puts "your guess is: '#{@guess}'"
+    @list_guesses = @list_guesses + @guess
+  end
+end
