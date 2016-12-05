0533f579f15f2ff8f004fa17c483d0fefb3132b6
Michael Laird <michaeljlaird@yahoo.com>
2016-12-04 21:08:33 -0800

    finished steps to game, now to creat loop to add all letters

--- ruby/gameSquirrels/game_mike.rb
+++ ruby/gameSquirrels/game_mike.rb
@@ -21,11 +21,15 @@
 # ask for guesses
   
  class Mike_game
+   
+  # attr_accessor @guess, @list_guesses
+   
   def initialize
     @test_toclean =""
     @guess = ""
     @list_guesses = ""
-    @hidden_word = ""
+    @to_guess = ""
+    @blank_spaces = ""
     @finish = false
   end
   
@@ -38,28 +42,60 @@
     @test_toclean
   end
 
-  def set_hidden_word
-    @hidden_word  = "-"*@testword.length
+  def set_hidden_word(word)
+    @blank_spaces  = "-"*word.length
   end
   
-  def receive_guess(test_toclean)
+  def receive_guess(test_toclean, list_guesses, blank_spaces, to_guess)
+    
     @guess = clean_text(test_toclean)[0]
-    puts "your guess is: '#{@guess}'"
+    @list_guesses = list_guesses
+    prev_guess
     @list_guesses = @list_guesses + @guess
+    @blank_spaces = blank_spaces
+    @to_guess = to_guess
+    string_blanksandfound
     @guess
   end
-end
 
-  # def clean_test
-  #   @testword = @testword.gsub(/[^a-zA-Z]/, '').downcase
-  #   if @testword.empty? == true
-  #     puts "Sorry, must include letters."
-  #     @finish = true
-  #   end
-  #   set_hidden_word
-  #   puts "Guess the letters to '#{@hidden_word}'"
-  # end
+  def prev_guess
+    if  @list_guesses.count(@guess) > 0
+      puts "you already guessed '{@guess}'."
+      @guess = ""
+    else
+      puts "your guess is: '#{@guess}'"
+    end
+  end       # end of prev_guess
+    
+    
+  def string_blanksandfound
+    if @guess != ""
+       if @to_guess.count(@guess) > 0
+         @to_guess.length.times do |x|
+           if @to_guess[x] == @guess then @blank_spaces[x] = @guess end
+          end
+          puts "good guess!"
+          p @blank_spaces
+        else
+          puts "can you even spell ?!?"
+        end
+    end
+  end           # end of string_blanksandfound
   
-  
-testing = Mike_game.new 
-puts testing.clean_text("  i lov(e   a 45 CAM. ")
+end       # end of class
+
+
+puts "what word do you want players to guess?"
+to_guess = gets.chomp
+testing = Mike_game.new  
+to_guess = testing.clean_text(to_guess)
+puts "the user must guess '#{to_guess}'."
+blank_spaces = testing.set_hidden_word(to_guess)
+
+list_guesses = ""
+
+p blank_spaces
+puts "Game player, what letter do you guess?"
+guess = gets.chomp
+testing.receive_guess(guess, list_guesses, blank_spaces, to_guess)
+puts "letter you guessed was #{guess}, previous guesses are #{list_guesses}, blanks are #{blank_spaces}, and original word is #{to_guess}"
