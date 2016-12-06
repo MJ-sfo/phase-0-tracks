e75ef31396fbe59d4f293fba57bb041164a1e91f
Michael Laird <michaeljlaird@yahoo.com>
2016-12-05 19:59:02 -0800

    finished changes with benjimon now need to answer questions in comments

--- ruby/gps6_squirrels/my_solution.rb
+++ ruby/gps6_squirrels/my_solution.rb
@@ -12,21 +12,29 @@ require_relative 'state_data'
 
 class VirusPredictor
 
+# when initialized, turns "on" the initiated variables - declares what kind of variables, and parameters they receive
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+# virus_effects - calls two other methods, sending data down to daughter methods. -NOT PRIVATE METHOD
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths #(@population_density, @population, @state)
+    speed_of_spread  #(@population_density, @state)
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+# predicted_deaths - PRIVATE - this method calculates death based on pop density and pop
+  def predicted_deaths  #(population_density, population, state)
     # predicted deaths is solely based on population density
+
+    # case @population_density     - could use CASE rather than IF-ELSIF
+    #   when @population_density >= 200 then number_of_deaths = (@population * 0.4).floor
+    #   when @population_density >= 150 then number_of_deaths = (@population * 0.3).floor
+    # end
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
@@ -42,11 +50,13 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-
-  def speed_of_spread(population_density, state) #in months
+  # speed_of_spread - calculates speed, based on pop density
+  def speed_of_spread   #(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
+
+
 
     if @population_density >= 200
       speed += 0.5
@@ -72,18 +82,22 @@ end
  # initialize VirusPredictor for each state
 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
 
+STATE_DATA.each do |state, hash_info|
+  info_state = VirusPredictor.new(state, hash_info[:population_density], hash_info[:population])
+  info_state.virus_effects
+end
 
 #=======================================================================
 # Reflection Section
