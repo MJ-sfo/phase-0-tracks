e9f2521a0ce726523f9698920a1b2a9b43493d89
Michael Laird <michaeljlaird@yahoo.com>
2016-12-09 10:19:00 -0800

    created file explore_sq.js, doesn't work yet

---
+++ js/explore_sq.js
@@ -0,0 +1,15 @@
+var str = "Loop over me!";
+var rev_string = "";
+
+function reverse() {
+  
+  console.log("does this work");
+  // for (var i = 0; i < str.length; i +=1) {   
+  //   console.log(str[i]);  
+  // }
+}
+
+// say hello
+function sayHello() {
+  console.log("Hello!");
+}
