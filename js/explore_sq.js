412c7a4ac2ec5f0cbf2bcab8ccb9d02449390d95
Michael Laird <michaeljlaird@yahoo.com>
2016-12-09 15:43:42 -0800

    added function

--- js/explore_sq.js
+++ js/explore_sq.js
@@ -3,13 +3,9 @@ var rev_string = "";
 
 function reverse() {
   
-  console.log("does this work");
-  // for (var i = 0; i < str.length; i +=1) {   
-  //   console.log(str[i]);  
-  // }
+  for (var i = 0; i < str.length; i +=1) {   
+    console.log(str[i]);  
+  }
 }
 
-// say hello
-function sayHello() {
-  console.log("Hello!");
-}
+ reverse() 
