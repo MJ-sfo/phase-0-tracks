09d106dfbc6a208aca91374d5a5217668a753549
Michael Laird <michaeljlaird@yahoo.com>
2016-12-09 18:04:23 -0800

    added last part - driver code that prints the variable if some condition is true

--- js/explore_sq.js
+++ js/explore_sq.js
@@ -1,13 +1,19 @@
-var str = "Loop over me!";
-var rev_string = "";
-
-function reverse() {
-  
+function reverse(str) {
+  var rev_string = "";
   for (var i = str.length - 1; i > -1; i += -1) {   
     console.log(str[i]);  
     rev_string = rev_string + str[i];
   }
-  console.log(rev_string);
+  // console.log(rev_string);
+  console.log("-------")
+  return rev_string;
 }
 
- reverse() 
+reverse("Happy test!") 
+
+// q5 - Add driver code that prints the variable if some condition is true.
+var long_text = reverse("In the beginning the word was with god and the word was god");
+if (long_text.length >10) {
+  console.log("The reverse is: " + long_text);
+}
+ 
