5a422ad1a825fc65061dba13476cd8b8be814b15
Michael Laird <michaeljlaird@yahoo.com>
2016-12-09 17:42:49 -0800

    answered research questions in release 3

--- js/data_structures_sq.js
+++ js/data_structures_sq.js
@@ -30,17 +30,44 @@ new_car.vroom();
 
 
 
-Release 3: Research on Your Own
-1- If you wanted to loop through the keys and values of an object, how would you do that? (There are a few ways to accomplish this, and some gotchas that can happen depending on your approach.)
-Googling for answers, the simplest method (or easiest to understand):
-var obj = {
-    first: "John", 
-    last: "Doe"
-};
+// Release 3: Research on Your Own
+// 1- If you wanted to loop through the keys and values of an object, how would you do that? (There are a few ways to accomplish this, and some gotchas that can happen depending on your approach.)
+// Googelling for answers, the simplest method (or easiest to understand):
+// var obj = {
+//     first: "John", 
+//     last: "Doe"
+// };
 
-Object.keys(obj).forEach(function(key) {
-    console.log(key, obj[key]);
-});
+// Object.keys(obj).forEach(function(key) {
+//     console.log(key, obj[key]);
+// });
 
 
-2- Are there advantages to using constructor functions to create objects? Disadvantages?
+// 2- Are there advantages to using constructor functions to create objects? Disadvantages?
+// A constructor is useful when you want to create multiple similar objects with the same properties and methods.
+
+// A constructor function is used with the new keyword 
+// var objFromConstructor = new ConstructorFunction();
+// Does not need to return a new instance of some object. 
+
+// A factory function is called like a "regular" function:
+// var objFromFactory = factoryFunction();   // please notice, no 'new'
+// But need to return a new instance of some object.  
+
+// EXAMPLES:
+// function ConstructorFunction() {
+//    this.someProp1 = "1";
+//    this.someProp2 = "2";
+// }
+// ConstructorFunction.prototype.someMethod = function() { /* whatever */ };
+
+// function factoryFunction() {
+//    var obj = {
+//       someProp1 : "1",
+//       someProp2 : "2",
+//       someMethod: function() { /* whatever */ }
+//    };
+//    // other code to manipulate obj in some way here
+//    return obj;
+// }
+
