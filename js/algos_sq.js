c577e302a1071abd792e2f5d7a24609549cf50a5
Michael Laird <michaeljlaird@yahoo.com>
2016-12-09 22:01:35 -0800

    pseudocode for Release 0

---
+++ js/algos_sq.js
@@ -0,0 +1,9 @@
+// Release 0: Find the Longest Phrase
+// pseudocode:
+// write a function
+// create lenght-array - cary text, value of lenght
+// itterate through array - if lenght of text longer than previous, save in enght-array
+// print answer
+// close function
+// driver - var several test-array's
+// call function with test-array's
