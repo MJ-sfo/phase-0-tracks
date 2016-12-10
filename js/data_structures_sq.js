8f17ad8ec421df011f1d5ed3b2287492d50a7329
Michael Laird <michaeljlaird@yahoo.com>
2016-12-09 17:27:51 -0800

    I was saving 7.2 onto explore_sq.js instead of data_structures_sq.js.  I corrected the two files

--- js/data_structures_sq.js
+++ js/data_structures_sq.js
@@ -1,7 +1,46 @@
 var colors= ["green", "beige", "yellow", "light-blue"];
 var names = ["Ed", "Chomper", "Blackie", "Charlie" ];
-
+var horse_colors = {};
 colors.push("Black");
 names.push("Dixie");
+for (var i = 0; i < names.length; i++){
+  horse_colors[names[i]] = colors[i];
+}
 console.log(colors);
 console.log(names);
+console.log(horse_colors);
+function Car(horsepower, torque, convertable) {
+  
+  console.log("Our new car:", this);
+  
+  this.horsepower = horsepower;
+  this.torque = torque;
+  this.convertable = convertable;
+  this.vroom = function() { console.log("*vroom-vroom*");};
+  
+  
+  console.log("car initialization complete!!");
+}
+var new_car = new Car(375, 300, true);
+console.log("this is the the car's horsepower: " + new_car.horsepower + ", and here is it's torque: " + new_car.torque);
+if (new_car.convertable) {
+  console.log("This is a convertible!");
+}
+new_car.vroom();
+
+
+
+Release 3: Research on Your Own
+1- If you wanted to loop through the keys and values of an object, how would you do that? (There are a few ways to accomplish this, and some gotchas that can happen depending on your approach.)
+Googling for answers, the simplest method (or easiest to understand):
+var obj = {
+    first: "John", 
+    last: "Doe"
+};
+
+Object.keys(obj).forEach(function(key) {
+    console.log(key, obj[key]);
+});
+
+
+2- Are there advantages to using constructor functions to create objects? Disadvantages?
--- js/explore_sq.js
+++ js/explore_sq.js
@@ -1,29 +1,13 @@
-var colors= ["green", "beige", "yellow", "light-blue"];
-var names = ["Ed", "Chomper", "Blackie", "Charlie" ];
-var horse_colors = {};
-colors.push("Black");
-names.push("Dixie");
-for (var i = 0; i < names.length; i++){
-  horse_colors[names[i]] = colors[i];
+var str = "Loop over me!";
+var rev_string = "";
+
+function reverse() {
+  
+  for (var i = str.length - 1; i > -1; i += -1) {   
+    console.log(str[i]);  
+    rev_string = rev_string + str[i];
+  }
+  console.log(rev_string);
 }
-console.log(colors);
-console.log(names);
-console.log(horse_colors);
-function Car(horsepower, torque, convertable) {
-  
-  console.log("Our new car:", this);
-  
-  this.horsepower = horsepower;
-  this.torque = torque;
-  this.convertable = convertable;
-  this.vroom = function() { console.log("*vroom-vroom*");};
-  
-  
-  console.log("car initialization complete!!");
-}
-var new_car = new Car(375, 300, true);
-console.log("this is the the car's horsepower: " + new_car.horsepower + ", and here is it's torque: " + new_car.torque);
-if (new_car.convertable) {
-  console.log("This is a convertible!");
-}
-new_car.vroom();
+
+ reverse() 
