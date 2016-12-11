069be2c1d9f9d207fb37c9bec16f8eedf57ba62b
Michael Laird <michaeljlaird@yahoo.com>
2016-12-10 18:35:11 -0800

    created funciton to compare two objects. Release 1 -q1

--- js/algos_sq.js
+++ js/algos_sq.js
@@ -27,3 +27,27 @@ console.log("The longest text is: " + longest_phrase(test_array));
 console.log("The longest text is: " + longest_phrase(test2));
 console.log("The longest text is: " + longest_phrase(test3));
 console.log("The longest text is: " + longest_phrase(text4));
+
+
+// Release 1: Find a Key-Value Match
+// pseudocode:
+// write function that takes two objects 
+// iterate through first object - in each cycle, iterate through second object,
+// see if share at least one key-value pair - if so return true
+// else, return false
+
+function key_value(object1, object2) {
+  var match = false;
+  var alertText = "";
+  for (i1=0; i1<object1.length; i1 +=1) {
+    for (i2 =0; i2<object2.length; i2 +=1) {
+
+      console.log("object1[i1] " + i1 + " is: " + object1[i1]  );
+      console.log("object2[i2] " + i2 + " is:" + object2[i2] );
+      if (object1[i1] == object2[i2]) {
+        match = true;
+      }  // if equal
+    }  // loop for i2
+  }  // loop for i1
+  return match;
+} // function key_value
