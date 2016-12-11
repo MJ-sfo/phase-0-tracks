5734abdebff091e650ce6d3e909f5c40b77e8d7e
Michael Laird <michaeljlaird@yahoo.com>
2016-12-10 17:21:10 -0800

    finished release 0, corrected method for finding longest text, ran 4 test variables

--- js/algos_sq.js
+++ js/algos_sq.js
@@ -11,12 +11,19 @@
 function longest_phrase(all_texts) {
   var record = ["", 0];
   for (i=0; i<all_texts.length; i +=1) {
-    if (all_texts[i].length > record[1]) {
+    if (all_texts[i].length >= record[1]) {
       record[0] = all_texts[i];
+      record[1] = all_texts[i].length;
     }
   }
   return record[0];
 }
 
 var test_array = ["miss", "Happy Days", "simmon", "This is the day the Lord has made"];
-console.log(longest_phrase(test_array));
+var test2 = ["Mom", 'Dad'];
+var test3 = [""];
+var text4 = ['nobody here but us chickens',' yes', 'today'];
+console.log("The longest text is: " + longest_phrase(test_array));
+console.log("The longest text is: " + longest_phrase(test2));
+console.log("The longest text is: " + longest_phrase(test3));
+console.log("The longest text is: " + longest_phrase(text4));
