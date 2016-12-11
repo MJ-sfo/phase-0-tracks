9c23e56eb81fc58ff5bacfcaa0909bf453c59d7b
Michael Laird <michaeljlaird@yahoo.com>
2016-12-10 17:08:00 -0800

    finished prob 2 of release 0-function finds longest text

--- js/algos_sq.js
+++ js/algos_sq.js
@@ -1,9 +1,22 @@
 // Release 0: Find the Longest Phrase
 // pseudocode:
 // write a function
-// create lenght-array - cary text, value of lenght
-// itterate through array - if lenght of text longer than previous, save in enght-array
+// create length-array - carry text, value of length
+// iterate through array - if length of text longer than previous, save in length-array
 // print answer
 // close function
 // driver - var several test-array's
 // call function with test-array's
+
+function longest_phrase(all_texts) {
+  var record = ["", 0];
+  for (i=0; i<all_texts.length; i +=1) {
+    if (all_texts[i].length > record[1]) {
+      record[0] = all_texts[i];
+    }
+  }
+  return record[0];
+}
+
+var test_array = ["miss", "Happy Days", "simmon", "This is the day the Lord has made"];
+console.log(longest_phrase(test_array));
